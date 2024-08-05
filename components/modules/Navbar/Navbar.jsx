0e544aa8159929d457608f0eec509532f94bc1b8
khadem-mh <khadem13359@gmail.com>
2024-08-05 15:58:49 -0700

    create menu responsive

--- components/modules/Navbar/Navbar.jsx
+++ components/modules/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import ListItem from "../ListItem/ListItem";
 import Link from "next/link";
 //
 import { GrContactInfo, GrArticle } from "react-icons/gr";
-import { RiCustomerService2Line, RiHome3Line } from "react-icons/ri";
+import { RiCustomerService2Line, RiHome3Line, RiMenu2Fill } from "react-icons/ri";
 import { MdOutlineContactSupport, MdOutlineShoppingBag } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
 
@@ -32,28 +32,41 @@ const Navbar = () => {
     const resizePageHandler = () => window.innerWidth <= 992 ? setIsShowNavbar('navbare__hidden') : setIsShowNavbar('navbare__show')
 
     return (
-        <nav className={`navbar__parent ${isShowNavbar}`}>
-            <ul className="navbar__right">
-                <ListItem text="Home" children={<RiHome3Line />} />
-                <ListItem text="About" children={<GrContactInfo />} />
-                <ListItem text="Services" children={<RiCustomerService2Line />} />
-            </ul>
+        <section>
+            {
+                isClient && isShowNavbar && window.innerWidth <= 992 &&
+                <menu className="navbar__menu">
+                    <Link href="#" className="navbar__logo">
+                        <Logo />
+                    </Link>
+                    <div className="navbar__menu-icon">
+                        <RiMenu2Fill/>
+                    </div>
+                </menu>
+            }
+            <nav className={`navbar__parent ${isShowNavbar}`}>
+                <ul className="navbar__right">
+                    <ListItem text="Home" children={<RiHome3Line />} />
+                    <ListItem text="About" children={<GrContactInfo />} />
+                    <ListItem text="Services" children={<RiCustomerService2Line />} />
+                </ul>
 
-            <div className="navbar__midd">
-                <div className="navbar__close" onClick={() => setIsShowNavbar(prev => !prev)}>
-                    <IoClose />
+                <div className="navbar__midd">
+                    <div className="navbar__close" onClick={() => setIsShowNavbar(prev => !prev)}>
+                        <IoClose />
+                    </div>
+                    <Link href="#" className="navbar__logo">
+                        <Logo />
+                    </Link>
                 </div>
-                <Link href="#" className="navbar__logo">
-                    <Logo />
-                </Link>
-            </div>
 
-            <ul className="navbar__left">
-                <ListItem text="Blog" children={<GrArticle />} />
-                <ListItem text="Contacts" children={<MdOutlineContactSupport />} />
-                <ListItem text="Buy" children={<MdOutlineShoppingBag />} />
-            </ul>
-        </nav>
+                <ul className="navbar__left">
+                    <ListItem text="Blog" children={<GrArticle />} />
+                    <ListItem text="Contacts" children={<MdOutlineContactSupport />} />
+                    <ListItem text="Buy" children={<MdOutlineShoppingBag />} />
+                </ul>
+            </nav>
+        </section>
     )
 }
 
