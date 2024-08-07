ff98fba24ae226d860bab5d796c486aeedde3e7b
khadem-mh <khadem13359@gmail.com>
2024-08-06 23:12:33 -0700

    create new template

---
+++ components/templates/WorkSample/workSample.jsx
@@ -0,0 +1,19 @@
+const WorkSample = () => {
+
+    return (
+        <article className="worksample__main">
+            <h2 className="worksample__title">Portfolio review</h2>
+            <nav className="worksample__nav">
+                <ul className="worksample__ul">
+                    <li className="worksample__li worksample__li-active">All</li>
+                    <li className="worksample__li">Branding</li>
+                    <li className="worksample__li">Print</li>
+                    <li className="worksample__li">Photography</li>
+                    <li className="worksample__li">Products</li>
+                </ul>
+            </nav>
+        </article>
+    )
+}
+
+export default WorkSample
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Header from "@/components/templates/Header/Header";
 import DescriptionBox from "@/components/modules/base/DescriptionBox/DescriptionBox";
+import WorkSample from "@/components/templates/WorkSample/workSample";
 
 export default function Home() {
   return (
@@ -12,7 +13,11 @@ export default function Home() {
       <section className="index__boxes">
         <DescriptionBox title={"High performance"} text={"Improve your site's performance and get higher rankings in Google."} srcVideo={"videos/func1.mp4"} />
         <DescriptionBox title={"The liveliest editor"} text={"No need for coding skills to customize and create ideal sites with Boo."} srcVideo={"videos/func2.mp4"} />
-        <DescriptionBox title={"Unique elements"} text={"Intense attention to detail is at the core of Bow's unique design."} srcVideo={"videos/func3.mp4"}/>
+        <DescriptionBox title={"Unique elements"} text={"Intense attention to detail is at the core of Bow's unique design."} srcVideo={"videos/func3.mp4"} />
+      </section>
+
+      <section>
+        <WorkSample/>
       </section>
 
       <br /><br /><br /><br />
