f203a18ca3f8270cdc58bcd04a7fe5966c059837
khadem-mh <khadem13359@gmail.com>
2024-08-13 13:25:41 -0700

    handle logic perspective 3d over article

--- components/templates/assets/Article/Article.jsx
+++ components/templates/assets/Article/Article.jsx
@@ -1,12 +1,45 @@
 import Link from "next/link";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useEffect } from "react";
 
 const Article = ({ title, text, srcImg }) => {
 
+    useEffect(() => {
+        const boxes = document.querySelectorAll('.article__link');
+
+        const handleMouseMove = (e, box) => {
+            const rect = box.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            const centerX = rect.width / 2;
+            const centerY = rect.height / 2;
+            const rotateX = (y - centerY) / centerY * 10;
+            const rotateY = (x - centerX) / centerX * 10;
+
+            box.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        };
+
+        const handleMouseLeave = (box) => {
+            box.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+        };
+
+        boxes.forEach(box => {
+            box.addEventListener('mousemove', (e) => handleMouseMove(e, box));
+            box.addEventListener('mouseleave', () => handleMouseLeave(box));
+        });
+
+        return () => {
+            boxes.forEach(box => {
+                box.removeEventListener('mousemove', (e) => handleMouseMove(e, box));
+                box.removeEventListener('mouseleave', () => handleMouseLeave(box));
+            });
+        };
+    }, []);
+
     return (
         <div className="article__main">
-            <img src={srcImg} alt="article-image" className="article__img"/>
-            <Link href="#" className="article__Link">
+            <img src={srcImg} alt="article-image" className="article__img" />
+            <Link href="#" className="article__link">
                 <h3 className="article__title">{title}</h3>
 
                 <p className="article__text">{text}</p>
@@ -20,4 +53,4 @@ const Article = ({ title, text, srcImg }) => {
     )
 }
 
-export default Article
+export default Article;
