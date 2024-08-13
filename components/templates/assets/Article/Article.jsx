f94e63f69c8ca4a5d1e99d43debb5d5286b6814a
khadem-mh <khadem13359@gmail.com>
2024-08-13 12:38:58 -0700

    styling card

--- components/templates/assets/Article/Article.jsx
+++ components/templates/assets/Article/Article.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-const Article = ({ title, text }) => {
+const Article = ({ title, text, srcImg }) => {
 
     return (
         <div className="article__main">
+            <img src={srcImg} alt="article-image" className="article__img"/>
             <Link href="#" className="article__Link">
                 <h3 className="article__title">{title}</h3>
 
