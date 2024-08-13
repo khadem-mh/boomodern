9edf1635d6eb6702224ede75c8cd0ae2466b4d7b
khadem-mh <khadem13359@gmail.com>
2024-08-12 23:34:01 -0700

    write styling for component latest content

--- components/templates/assets/Article/Article.jsx
+++ components/templates/assets/Article/Article.jsx
@@ -7,10 +7,12 @@ const Article = ({ title, text }) => {
         <div className="article__main">
             <Link href="#" className="article__Link">
                 <h3 className="article__title">{title}</h3>
+
                 <p className="article__text">{text}</p>
+
                 <div className="article__footer">
-                    <button className="article__btn">read more</button>
-                    <FaArrowRightLong className="article__arrow"/>
+                    <span className="article__btn">read more</span>
+                    <FaArrowRightLong className="article__arrow" />
                 </div>
             </Link>
         </div>
