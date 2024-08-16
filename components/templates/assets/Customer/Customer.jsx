a093fc4654986446735396e70767bb8b8a7842eb
khadem-mh <khadem13359@gmail.com>
2024-08-15 22:20:43 -0700

    ok

--- components/templates/assets/Customer/Customer.jsx
+++ components/templates/assets/Customer/Customer.jsx
@@ -1,15 +1,15 @@
 
-const Customer = ({imgSrc, name, desc, styles, onClickCustomer, index}) => {
+const Customer = ({ imgSrc, name, desc }) => {
 
     return (
-        <div className={`customer__main ${styles}`} onClick={() => onClickCustomer(index)}>
+        <div className={`customer__main`}>
             <div className="customer__left">
-                <img src="img/quote.png" alt="quote" className="customer__quote"/>
+                <img src="img/quote.png" alt="quote" className="customer__quote" />
                 <p className="customer__desc">{desc}</p>
                 <p className="customer__name">- {name}</p>
             </div>
             <div className="customer__right">
-                <img src={`img/${imgSrc}`} alt="customer-image" className="customer__img"/>
+                <img src={`img/${imgSrc}`} alt="customer-image" className="customer__img" />
             </div>
         </div>
     )
--- components/templates/base/Customers/Customers.jsx
+++ components/templates/base/Customers/Customers.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react"
 import Customer from "../../assets/Customer/Customer"
 import customerInfos from "@/data/customers.json"
 
 const Customers = () => {
-
-    const [isShowCustomer, setIsShowCustomer] = useState('2')
-
-    const changeActiveCustomer = index => {
-        setIsShowCustomer(index)
-    }
 
     return (
         <div className="customers__main">
@@ -18,26 +11,7 @@ const Customers = () => {
             <div className="customers__slider">
                 {
                     customerInfos.map((item, index) => (
-                        <Customer
-                            key={index}
-                            {...item}
-                            styles={`customers__normal ${isShowCustomer !== item.index
-                                ?
-                                `hidden cursor-pointer ${isShowCustomer == 1 && item.index == 2
-                                    ?
-                                    'customers__hidden-2-left'
-                                    :
-                                    isShowCustomer == 3 && item.index == 2
-                                        ?
-                                        'customers__hidden-2-right'
-                                        :
-                                        ''
-                                }`
-                                :
-                                `cursor-default customers__active-${item.index}`}`
-                            }
-                            onClickCustomer={changeActiveCustomer}
-                        />
+                        <Customer key={index} {...item} />
                     ))
                 }
             </div>
